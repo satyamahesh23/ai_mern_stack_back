34f919d7e0b2b70d6b7e05ee1c89262597364c6b
satyamahesh23 <satyamahesh223@gmail.com>
2026-07-27 19:35:36 +0530

    changed

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb=async()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("database connect")
 
     } catch(error){
